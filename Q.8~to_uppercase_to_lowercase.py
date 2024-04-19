a35a49cab2218c1d57eef2ff3d43f34b43c6e69b
amanjha2000 <167403087+amanjha2000@users.noreply.github.com>
2024-04-19 09:52:02 +0530

    Add files via upload

---
+++ Q.8~to_uppercase_to_lowercase.py
@@ -0,0 +1,8 @@
+# write a code to convert a given string to uppercase or lowercase.
+
+def to_uppercase(s):
+    return s.upper()
+def to_lowercase(s):
+    return s.lower()
+print(to_uppercase("hello"))
+print(to_lowercase("WORLD"))

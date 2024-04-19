0ca6dbef3adef97043532443c8ccf571c49ffb3d
amanjha2000 <167403087+amanjha2000@users.noreply.github.com>
2024-04-19 09:49:17 +0530

    Add files via upload

---
+++ Q.2~count_vowels.py
@@ -0,0 +1,7 @@
+# write a code to count the number of vowels in a string.
+
+def count_vowels(s):
+    vowels = 'aeiouAEIOU'
+    return sum (1 for char in s if char in vowels)
+s = "hello world"
+print(count_vowels(s))

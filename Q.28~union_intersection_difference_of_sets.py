0e3e1277ba96e808cba0f2c33279a89495826479
amanjha2000 <167403087+amanjha2000@users.noreply.github.com>
2024-04-19 10:01:19 +0530

    Add files via upload

---
+++ Q.28~union_intersection_difference_of_sets.py
@@ -0,0 +1,12 @@
+# create a code that define two sets of integers. Then, print the union ,intersection ,and difference of these two sets.
+
+set1 = {1,2,3,4,5}
+set2 = {4,5,6,7,8}
+
+union_set = set1.union(set2)
+intersection_set = set1.intersection(set2)
+difference_set = set1.difference(set2)
+
+print("the union of the two sets is :",union_set)
+print("the intersection of the two set is :",intersection_set)
+print("the difference of the two set is :",difference_set)
